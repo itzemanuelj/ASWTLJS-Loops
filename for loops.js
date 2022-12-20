658a283e8e313580f2ddaac54d800d720c2f5509
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-20 13:21:59 -0500

    created for loops.js

---
+++ for loops.js
@@ -0,0 +1,9 @@
+///Code a loop using the usual conventions and < that tests each element of a 5-element array, answers, to see if it has the value "yes". 
+///If so, a variable, already declared, is assigned a positive, single-digit integer. Use a number, not the array length, to limit the number of loops.
+
+for (var i = 0; i < 5; i++) {
+    if (answers[i] === "yes") {
+      positive = 1;
+    }
+  }
+  
